acc70f71680033948b91677f949828ef35ff1c3a
Gav Barosee <barbarosee@gmail.com>
2020-03-17 11:33:24 +0400

    Setup basic structure + routes

---
+++ src/routes/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import ProductDetails from "../components/ProductDetails";
+import ProductsList from "../components/ProductsList";
+import Cart from "../components/Cart";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Route path="/" exact component={ProductsList} />
+      <Route path="/product/:slug/:id" component={ProductDetails} />
+      <Route path="/cart" exact component={Cart} />
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
